abb968155742815eedfed11bc3140f539b1a02cb
baconstarvation <ant.ucd@gmail.com>
2015-03-27 21:34:34 -0700

    Commit 9-5-2 Added tests to the package

--- packages/threshio:errors/package.js
+++ packages/threshio:errors/package.js
@@ -14,3 +14,10 @@ Package.onUse(function (api, where) {
   if (api.export) 
     api.export('Errors');
 });
+
+Package.onTest(function(api) {
+	api.use('threshio:errors', 'client');
+	api.use(['tinytest', 'test-helpers'], 'client');
+
+	api.addFiles('errors_tests.js', 'client');
+});
